9b2c8985a93755c670afb2d48ece2c27514af6e4
Witalij <witalij.rapicki@gmail.com>
2026-01-19 13:28:24 +0100

    feat: terraform fmt

--- monitoring.tf
+++ monitoring.tf
@@ -3,14 +3,14 @@ resource "aws_sns_topic" "alerts" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "alb_5xx" {
-  alarm_name = "alb-5xx-errors"
+  alarm_name          = "alb-5xx-errors"
   comparison_operator = "GreaterThanThreshold"
-  evaluation_periods = 1
-  threshold = 5
-  period = 60
-  statistic = "Sum"
+  evaluation_periods  = 1
+  threshold           = 5
+  period              = 60
+  statistic           = "Sum"
 
-  namespace = "AWS/ApplicationELB"
+  namespace   = "AWS/ApplicationELB"
   metric_name = "HTTPCode_ELB_5cc_Count"
 
   dimensions = {
@@ -21,14 +21,14 @@ resource "aws_cloudwatch_metric_alarm" "alb_5xx" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "asg_no_instances" {
-  alarm_name = "asg-no-inservice-instances"
+  alarm_name          = "asg-no-inservice-instances"
   comparison_operator = "LessThanThreshold"
-  evaluation_periods = 1
-  threshold = 1
-  period = 60
-  statistic = "Minimum"
+  evaluation_periods  = 1
+  threshold           = 1
+  period              = 60
+  statistic           = "Minimum"
 
-  namespace = "AWS/AutoScaling"
+  namespace   = "AWS/AutoScaling"
   metric_name = "GroupInServiceInstances"
 
   dimensions = {
@@ -36,18 +36,18 @@ resource "aws_cloudwatch_metric_alarm" "asg_no_instances" {
 
   }
 
-  alarm_actions = [ aws_sns_topic.alerts.arn ]
+  alarm_actions = [aws_sns_topic.alerts.arn]
 }
 
 resource "aws_cloudwatch_metric_alarm" "cpu_high" {
-  alarm_name = "ec2-cpu-high"
+  alarm_name          = "ec2-cpu-high"
   comparison_operator = "GreaterThanThreshold"
-  evaluation_periods = 1
-  threshold = 70
-  period = 60
-  statistic = "Average"
+  evaluation_periods  = 1
+  threshold           = 70
+  period              = 60
+  statistic           = "Average"
 
-  namespace = "AWS/EC2"
+  namespace   = "AWS/EC2"
   metric_name = "CPUUtilization"
 
   dimensions = {
